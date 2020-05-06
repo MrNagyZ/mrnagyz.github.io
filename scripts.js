cf7a6ac2585fdc3d2858df6ed0cd55c2d7c6e31c
fraktalDS <fraktal79@gmail.com>
2020-05-06 20:57:55 +0200

    Update 7

--- scripts.js
+++ scripts.js
@@ -15,6 +15,28 @@ let photodata = [
     leiras: 'Ez egy rendezvéyfotó 3'
 }];
 let ID = 0;
+let loadPhoto = (ID) => {
 $('#nagykep').attr('src', photodata[ID].photo);
 $('#cim').text(photodata[ID].title);
 $('#leiras').text(photodata[ID].leiras);
+  }
+
+  loadPhoto(ID);
+
+  $('#jobb').click(() => {
+    ID++;
+    if (ID === 3) {
+        ID = 0;
+    }
+
+    loadPhoto(ID);
+  })
+  $('#bal').click(() => {
+    ID--;
+    if (ID === -1) {
+        ID = 2;
+    }
+    loadPhoto(ID);
+  })
+
+ 
